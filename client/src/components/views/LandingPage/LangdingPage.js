ee468c2e4caf9b337f505ca8f0815711bb11eb4b
tn841 <tn841@naver.com>
2020-07-21 18:20:41 +0900

    #32. 로그아웃 구현

--- client/src/components/views/LandingPage/LangdingPage.js
+++ client/src/components/views/LandingPage/LangdingPage.js
@@ -1,11 +1,22 @@
 import React, {useEffect} from 'react'
 import axios from 'axios';
-function LangdingPage() {
+function LangdingPage(props) {
 
     useEffect(() => {
         axios.get('/api/hello')
         .then(response => console.log(response.data))
     }, [])
+
+    const onClickHander = (event) => {
+        axios.get('/api/user/logout')
+        .then(res => {
+            if(res.data.success){
+                props.history.push('/login')
+            } else {
+                alert('로그아웃 실패');
+            }
+        })
+    }
 
     return (
         <div style={{
@@ -13,6 +24,10 @@ function LangdingPage() {
             , width: '100%', height: '100vh'
         }}>
             <h2>시작 페이지</h2>
+
+            <button onClick={onClickHander}>
+                로그아웃
+            </button>
         </div>
     )
 }
